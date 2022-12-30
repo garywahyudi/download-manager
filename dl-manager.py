59e1e9a08ba46ccf4034c24c83d476318c581477
Gary Wahyudi <garywahyudi.2001@Gmail.com>
2022-12-30 12:31:06 +0700

    Update dl-manager.py
    Added progress bar, and better terminal command syntaxs

--- dl-manager.py
+++ dl-manager.py
@@ -1,36 +1,38 @@
-import tkinter as tk
-import tkinter.filedialog
-import requests
+import click
 import os
+import requests
+import shutil
 
-# Create a tkinter window
-root = tk.Tk()
-root.withdraw()
+@click.command()
+@click.option("--url", required=True, help="URL of the file to download")
+@click.option("--file", required=True, help="local file path to save the file")
+def download_file(url, file):
+    # Create the local directory if it does not already exist
+    if not os.path.exists(os.path.dirname(file)):
+        os.makedirs(os.path.dirname(file))
 
-# Prompt the user for the URL of the file they want to download
-url = input("Enter the URL of the file you want to download: ")
+    # Use the 'get' method of the 'requests' module to download the file
+    response = requests.get(url, stream=True)
 
-# Use the tkinter filedialog to allow the user to choose the local directory
-# where they want to save the file
-local_directory = tk.filedialog.askdirectory()
+    # Check for any HTTP errors and raise an exception if there is an error
+    response.raise_for_status()
 
-# Prompt the user for the local file name
-local_filename = input("Enter the local file name (REMEMBER TO PUT THE FILE TYPE): ")
+    # Get the total file size
+    total_size = int(response.headers.get("Content-Length", 0))
 
-# Create the full local file path by combining the directory and file name
-local_filepath = os.path.join(local_directory, local_filename)
+    # Open the local file in 'write binary' mode and write the contents of the
+    # downloaded file to it
+    with open(file, 'wb') as f:
+        for chunk in response.iter_content(chunk_size=1024):
+            # Write the chunk to the file
+            f.write(chunk)
+            # Print the progress bar to the console
+            downloaded = f.tell()
+            progress = downloaded / total_size
+            print(f"\r[{'#' * int(30 * progress):<30}] {int(progress * 100)}%", end="", flush=True)
 
-# Create the local directory if it does not already exist
-if not os.path.exists(os.path.dirname(local_filepath)):
-    os.makedirs(os.path.dirname(local_filepath))
+    # Print a completion message
+    print(f"{url} download complete")
 
-# Use the 'get' method of the 'requests' module to download the file
-response = requests.get(url)
-
-# Check for any HTTP errors and raise an exception if there is an error
-response.raise_for_status()
-
-# Open the local file in 'write binary' mode and write the contents of the
-# downloaded file to it
-with open(local_filepath, 'wb') as f:
-    f.write(response.content)
+if __name__ == "__main__":
+    download_file()
